f9e7922238bca6ce43edd40f8a5f0bc3c407febe
FIRST_NAME LAST_NAME <gabomurut@gmail.com>
2021-03-18 18:15:41 -0300

    Final

--- src/main/java/com/rugbyplayers/rugbyplayers/controller/PlayerListController.java
+++ src/main/java/com/rugbyplayers/rugbyplayers/controller/PlayerListController.java
@@ -1,19 +1,12 @@
 package com.rugbyplayers.rugbyplayers.controller;
 
-import javax.persistence.OrderBy;
-
 import com.rugbyplayers.rugbyplayers.RugbyPlayer;
 import com.rugbyplayers.rugbyplayers.RugbyPlayerRepository;
-
-import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
